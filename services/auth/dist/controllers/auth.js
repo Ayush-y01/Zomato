c3bfb22ea6e76d11f2cb9087b95725cb15ee2d39
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-03-14 23:43:14 +0530

    create middleware

--- services/auth/dist/controllers/auth.js
+++ services/auth/dist/controllers/auth.js
@@ -1,25 +1,20 @@
 import User from "../model/User.js";
 import TryCatch from "../middlewares/trycatch.js";
-import jwt from 'jsonwebtoken'
-
+import jwt from 'jsonwebtoken';
 export const loginUser = TryCatch(async (req, res) => {
-    const { email, name, picture } = req.body
-
-    let user = await User.findOne({ email })
-
+    const { email, name, picture } = req.body;
+    let user = await User.findOne({ email });
     if (!user) {
         user = await User.create({
             name,
             email,
             image: picture
-        })
+        });
     }
-
-    const token = jwt.sign({ user }, process.env.JWT_SECRET as string, { expiresIn: "15d" })
-
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "15d" });
     res.status(200).json({
-        message:"Logged Succssfully",
+        message: "Logged Succssfully",
         token,
         user
-    })
-})
+    });
+});
