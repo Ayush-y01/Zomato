6ac02d71858dd4498d4643e49e35762f2ca89053
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-03-20 20:38:00 +0530

    complete google auth login system with protected route and role assign page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast"
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import SelectRole from "./pages/SelectRole";
+import Navbar from "./components/Navbar";
+import Account from "./pages/Account";
+
+
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/select-role" element={<SelectRole />} />
+          <Route path="/account" element={<Account />} />
+
+        </Route>
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,91 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import { authService } from "../main";
+import {type  LocationData, type AppContextType, type User } from "../types";
+
+const AppContext = createContext<AppContextType | undefined>(undefined)
+
+interface AppProviderProps {
+    children: ReactNode;
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+    const [user, setUser] = useState<User | null >(null)
+    const [isAuth, setIsAuth] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    const [location, setLocation] = useState<LocationData | null>(null)
+    const [loadingLocation, setLoadingLocation] = useState(false)
+    const [city, setCity] = useState("featching location..")
+
+
+    async function fetchUser() {
+        try {
+            const token = localStorage.getItem("token")
+
+            const { data } = await axios.get(`${authService}/api/auth/me`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+
+            })
+            setUser(data)
+            setIsAuth(true);
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchUser();
+    },[]);
+
+    useEffect(() => {
+        if (navigator.geolocation) return;
+        setLoadingLocation(true);
+
+        navigator.geolocation.getCurrentPosition(async (position)=>{
+            const {latitude, longitude} = position.coords;
+
+            try {
+                const res = await  fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`)
+
+                const data = await res.json()
+
+                setLocation({
+                    latitude,
+                    longitude,
+                    formattedAddress: data.display_nmae || "current location"
+                })
+
+                setCity(
+                    data.address.city || data.address.town || data.address.village || "your location"
+                )
+
+            } catch (error) {
+                setLocation({
+                    latitude,
+                    longitude,
+                    formattedAddress:"current location"
+                })
+                setCity("failed to load")
+            }
+        })
+    },[])
+    
+    return (
+    <AppContext.Provider 
+        value={{isAuth, loading,setUser,setIsAuth,setLoading,user,location, loadingLocation,city}}>
+            {children}
+    </AppContext.Provider>)
+}
+
+export const useAppData = (): AppContextType => {
+    const context = useContext(AppContext)
+    if (!context) {
+        throw new Error("useAppData must be used within AppProvider")
+    }
+    return context
+}
