90a3e5da58b64e6ef9e673db18df445e0db13e54
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-04-19 19:30:17 +0530

    create cart controller and route

---
+++ services/restaurent/src/controllers/cart.ts
@@ -0,0 +1,168 @@
+import mongoose from "mongoose";
+import { AuthenticationRequest } from "../middlewares/isAuth.js";
+import TryCatch from "../middlewares/trycatch.js";
+import Carts from "../models/Carts.js";
+
+export const addToCart = TryCatch(async (req: AuthenticationRequest, res) => {
+    if (!req.user) {
+        return res.status(404).json({
+            message: "Please Login"
+        })
+    }
+
+    const userId = req.user._id;
+    const { restaurantId, itemId } = req.body;
+
+    if (
+        !mongoose.Types.ObjectId.isValid(restaurantId) ||
+        !mongoose.Types.ObjectId.isValid(itemId)
+    ) {
+        return res.status(400).json({
+            message: "Invalid restaurant and item"
+        })
+    }
+
+    const cartFromDifferentRestaurant = await Carts.findOne({
+        userId,
+        restaurantId: { $ne: restaurantId },
+    })
+
+    if (cartFromDifferentRestaurant) {
+        return res.status(400).json({
+            message: "Please Clear your Cart first"
+        })
+    }
+
+    const cartItem = await Carts.findOneAndUpdate(
+        { userId, restaurantId, itemId },
+        {
+            $inc: { quantity: 1 },
+            $setOnInsert: { userId, restaurantId, itemId },
+        },
+        {
+            upsert: true,
+            returnDocument: "after",
+            setDefaultsOnInsert: true
+        }
+    )
+
+    return res.json({
+        message: "Item added to Cart",
+        cart: cartItem,
+    })
+})
+
+export const fetchCart = TryCatch(async(req:AuthenticationRequest, res) => {
+    if (!req.user) {
+        return res.status(400).json({
+            message:"Please login"
+        })
+    }
+
+    const userId = req.user._id
+    
+    const cartItems = await Carts.find({userId}).populate("itemId").populate("restaurantId")
+    
+
+    let subTotal = 0
+    let cartLength = 0
+
+    for(const cartItem of cartItems){
+        const item:any = cartItem.itemId;
+
+        subTotal += item.price * cartItem.quantity
+        cartLength += cartItem.quantity
+
+    }
+
+    return res.json({
+        success:true,
+        subTotal,
+        cartLength,
+        cart:cartItems,
+    })
+})
+
+export const incrementCartItem = TryCatch(async(req:AuthenticationRequest, res) => {
+    const userId = req.user?._id
+
+    const {itemId} = req.body
+
+    if (!userId || !itemId) {
+        return res.status(400).json({
+            message: "Invalid Request"
+        })
+    }
+
+    const cartItem = await Carts.findOneAndUpdate(
+        {userId, itemId},
+        {$inc: {quantity: 1} },
+        {new: true}
+    )
+
+    if (!cartItem) {
+        return res.status(404).json({
+            message: "Item not Found!!! "
+        })
+    }
+
+    res.json({
+        message:"Quantity increased",
+        cartItem
+    })
+})
+
+export const decrementCartItem = TryCatch(async(req:AuthenticationRequest, res) => {
+    const userId = req.user?._id
+
+    const {itemId} = req.body
+
+    if (!userId || !itemId) {
+        return res.status(400).json({
+            message: "Invalid Request"
+        })
+    }
+
+    const cartItem = await Carts.findOne(
+        {userId, itemId},
+    )
+
+    if (!cartItem) {
+        return res.status(404).json({
+            message: "Item not Found!!! "
+        })
+    }
+
+    if(cartItem.quantity === 1){
+        await Carts.deleteOne({userId, itemId})
+
+        return res.json({
+            message:"Item remove from Cart"
+        })
+    }
+
+    cartItem.quantity -= 1;
+    await cartItem.save()
+
+    res.json({
+        message:"Quantity decreased",
+        cartItem
+    })
+})
+
+export const clearCart = TryCatch(async(req:AuthenticationRequest, res) => {
+    const userId = req.user?._id
+
+    if (!userId) {
+        return res.status(400).json({
+            message: "Unauthorize"
+        })
+    }
+
+    await Carts.deleteMany({userId})
+
+    res.json({
+        message:"Cart Cleared",
+    })
+})
+
