7d0b58713870c3fa47410129ed6cfd5917530c91
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-05-04 19:44:32 +0530

    create route to verify payment system

--- services/utils/dist/index.js
+++ services/utils/dist/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import uploadRoutes from "./routes/cloudinary.js"; // (we’ll rename later if you want)
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+import PaymentRouter from './routes/payment.js';
 dotenv.config();
+connectRabbitMQ();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-// 🔥 ImageKit ENV check
 const { IMAGEKIT_PUBLIC_KEY, IMAGEKIT_PRIVATE_KEY, IMAGEKIT_URL_ENDPOINT, } = process.env;
-console.log("🔑 ImageKit Config:", {
+console.log(" ImageKit Config:", {
     publicKey: IMAGEKIT_PUBLIC_KEY ? "EXISTS" : "MISSING",
     privateKey: IMAGEKIT_PRIVATE_KEY ? "EXISTS" : "MISSING",
     urlEndpoint: IMAGEKIT_URL_ENDPOINT,
@@ -17,8 +19,8 @@ console.log("🔑 ImageKit Config:", {
 if (!IMAGEKIT_PUBLIC_KEY || !IMAGEKIT_PRIVATE_KEY || !IMAGEKIT_URL_ENDPOINT) {
     throw new Error("Missing ImageKit env variables");
 }
-// routes
 app.use("/api", uploadRoutes);
+app.use("/api/payment", PaymentRouter);
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
     console.log(`🚀 Utils service running on ${PORT}`);
---
+++ services/utils/src/routes/payment.ts
@@ -0,0 +1,10 @@
+import express  from "express";
+import { createRazorpayorder, verifyRazorpayPayment } from "../controllers/payment.js";
+
+const router = express.Router()
+
+router.post("/create", createRazorpayorder);
+router.post("/verify", verifyRazorpayPayment)
+
+
+export default router
