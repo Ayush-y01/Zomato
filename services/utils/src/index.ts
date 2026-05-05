010d71f1a50925582c0649af3656bddb151f696b
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-05-05 20:18:22 +0530

    update utils file

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import uploadRoutes from "./routes/cloudinary.js"; // (we’ll rename later if you want)
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+import PaymentRouter from './routes/payment.js'
 
 dotenv.config();
+
+connectRabbitMQ()
 
 const app = express();
 
@@ -12,14 +16,13 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// 🔥 ImageKit ENV check
 const {
   IMAGEKIT_PUBLIC_KEY,
   IMAGEKIT_PRIVATE_KEY,
   IMAGEKIT_URL_ENDPOINT,
 } = process.env;
 
-console.log("🔑 ImageKit Config:", {
+console.log(" ImageKit Config:", {
   publicKey: IMAGEKIT_PUBLIC_KEY ? "EXISTS" : "MISSING",
   privateKey: IMAGEKIT_PRIVATE_KEY ? "EXISTS" : "MISSING",
   urlEndpoint: IMAGEKIT_URL_ENDPOINT,
@@ -29,8 +32,8 @@ if (!IMAGEKIT_PUBLIC_KEY || !IMAGEKIT_PRIVATE_KEY || !IMAGEKIT_URL_ENDPOINT) {
   throw new Error("Missing ImageKit env variables");
 }
 
-// routes
 app.use("/api", uploadRoutes);
+app.use("/api/payment", PaymentRouter);
 
 const PORT = process.env.PORT || 5002;
 
