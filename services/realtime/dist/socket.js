05f340b86e07daff02e2aa7a0fa35a2a4c7a06a9
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-05-18 21:34:13 +0530

    create real time system for real time update

---
+++ services/realtime/dist/socket.js
@@ -0,0 +1,52 @@
+import { Server } from "socket.io";
+import jwt from 'jsonwebtoken';
+let io;
+export const initSocket = (server) => {
+    io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
+    });
+    io.use((socket, next) => {
+        try {
+            const token = socket.handshake.auth?.token;
+            if (!token) {
+                return next(new Error("Umauthorized"));
+            }
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            if (!decoded || !decoded.user) {
+                return next(new Error("Umauthorized"));
+            }
+            socket.data.user = decoded.user;
+            next();
+        }
+        catch (error) {
+            console.log("❌ socket auth failed: ", error);
+            next(new Error("Umauthorized"));
+        }
+    });
+    io.on("connection", (socket) => {
+        const user = socket.data.user;
+        if (!user) {
+            socket.disconnect();
+            return;
+        }
+        const userId = user._id;
+        socket.join(`user: ${userId}`);
+        if (user.restaurantId) {
+            socket.join(`Restaurant: ${user.restaurantId}`);
+        }
+        console.log(`user Connected: ${userId}`);
+        console.log("Socket Room ", [...socket.rooms]);
+        socket.on("disconnect", () => {
+            console.log(`User Disconnected: ${userId}`);
+        });
+    });
+    return io;
+};
+export const getIO = () => {
+    if (!io) {
+        throw new Error("Socket.io not initialized");
+    }
+    return io;
+};
