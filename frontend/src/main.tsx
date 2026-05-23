d967e0970e464a6cc3f8f5326751369e4e2fd945
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-05-23 22:06:23 +0530

    some minor updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,6 +10,7 @@ export const authService = "http://localhost:5000";
 export const restaurantService = "http://localhost:5001";
 export const utilsService = "http://localhost:5002";
 export const RealtimeService = "http://localhost:5004";
+export const RiderService = "http://localhost:5005";
 
 createRoot(document.getElementById('root')!).render(
     <GoogleOAuthProvider clientId="284911799169-8e8vg0jufb5pmo0ee8ging0cnbfechd0.apps.googleusercontent.com">
