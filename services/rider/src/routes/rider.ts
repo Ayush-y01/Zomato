7ae5d208c41e64768ad68680a3c85adfc34b8a47
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-05-29 20:12:04 +0530

    create route for rider

--- services/rider/src/routes/rider.ts
+++ services/rider/src/routes/rider.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import { isAuth } from "../middlewares/isAuth.js"
-import { fetchMyProfile, toggleRiderAvailbility } from "../controllers/rider.js"
+import { addRiderProfile, fetchMyProfile, toggleRiderAvailbility } from "../controllers/rider.js"
+import uploadFile from "../middlewares/multer.js"
 
 const router = express.Router()
+
+router.post("/new", isAuth, uploadFile,addRiderProfile)
 
 router.get("/myprofile", isAuth, fetchMyProfile)
 router.patch("/toggle",isAuth,toggleRiderAvailbility )
